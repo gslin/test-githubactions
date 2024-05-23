b8b0c010144f0a27089cda16bb9ed8135095c5a1
Gea-Suan Lin <gslin@gslin.org>
2024-05-24 03:21:15 +0800

    Use ejs to render.

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Hello, world').end();
+  res.render('index');
 });
 
 app.listen(port, () => {
