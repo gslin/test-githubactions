12c7123cab6b12991ed1964b8e121666107f63c8
Gea-Suan Lin <gslin@gslin.org>
2024-05-24 02:55:01 +0800

    Implement index.js.

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hello, world').end();
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
