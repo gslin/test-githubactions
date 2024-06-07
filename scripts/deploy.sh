1044949b90559dcdba3d5a9adc26ee0326fd4fb4
Gea-Suan Lin <gslin@gslin.org>
2024-06-07 13:13:55 +0800

    Use --shell=bash to workaround this.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,10 @@
 #!/bin/bash -x
 
-# Show all environments.
-export
-
 # Install fnm if not installed.
 test -x ~/.fnm/fnm || ( cd /tmp; rm -f fnm-linux.zip; wget -c https://github.com/Schniz/fnm/releases/download/v1.37.0/fnm-linux.zip; mkdir ~/.fnm; unzip fnm-linux.zip -d ~/.fnm/; chmod 755 ~/.fnm/fnm )
 
 # Load fnm.
-eval "$(~/.fnm/fnm env --use-on-cd)"
+eval "$(~/.fnm/fnm env --shell=bash --use-on-cd)"
 export PATH="${HOME}/.fnm:${PATH}"
 
 # Install node 20 if not installed, also set to default.
