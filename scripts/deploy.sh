38831d8f7493321cc58a7b32f0922953fb95b2d8
Gea-Suan Lin <gslin@gslin.org>
2024-06-07 13:08:12 +0800

    Use deploy.sh.

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install fnm if not installed.
+test -x ~/.fnm/fnm || ( cd /tmp; rm -f fnm-linux.zip; wget -c https://github.com/Schniz/fnm/releases/download/v1.37.0/fnm-linux.zip; mkdir ~/.fnm; unzip fnm-linux.zip -d ~/.fnm/; chmod 755 ~/.fnm/fnm )
+
+# Load fnm.
+eval "$(~/.fnm/fnm env --use-on-cd)"
+export PATH="${HOME}/.fnm:${PATH}"
+
+# Install node 20 if not installed, also set to default.
+fnm default 20 || ( fnm install 20; fnm default 20 )
+
+cd ~/test-githubactions; npx pm2 restart ecosystem.config.js
