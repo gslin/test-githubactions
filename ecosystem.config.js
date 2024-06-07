b68dafc360349339b4c63c40cdf1ac44f685725a
Gea-Suan Lin <gslin@gslin.org>
2024-06-07 11:36:25 +0800

    Try to use pm2 to run service.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   apps: [{
-    name: "server",
-    script: "./server.js",
-    env: {
-      NODE_ENV: "development",
-    },
+    name: 'server.js',
+    script: './server.js',
     env_production: {
-      NODE_ENV: "production",
+      NODE_ENV: 'production',
+    },
+    env: {
+      NODE_ENV: 'development',
     }
   }],
 };
